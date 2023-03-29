9e71a5ef1a726ec00c5763214bf3fc87b4a22880
John <bosass80@gmail.com>
2023-03-29 05:13:43 -0700

    Committed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,19 +1,21 @@
--- Creates a MySQL server with:
---   Database hbnb_dev_db.
---   User hbnb_dev with password hbnb_dev_pwd in localhost.
---   Grants all privileges for hbnb_dev on hbnb_dev_db.
---   Grants SELECT privilege for hbnb_dev on performance_schema.
+#!/bin/bash
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-FLUSH PRIVILEGES;
+# MySQL credentials
+MYSQL_ROOT_USER="root"
+MYSQL_ROOT_PASSWORD="your_root_password"
+
+# Database and user credentials
+DB_NAME="hbnb_dev_db"
+DB_USER="hbnb_dev"
+DB_PASSWORD="hbnb_dev_pwd"
+
+# Create database if it does not exist
+mysql -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+
+# Create user and grant privileges
+mysql -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';"
+mysql -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "GRANT SELECT ON performance_schema.* TO '${DB_USER}'@'localhost';"
+
+# Flush privileges
+mysql -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
